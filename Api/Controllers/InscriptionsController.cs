c65a6eb04a10c543673cf6b047f0c8ffc15506d3
Moises27Dev <sesemoi27@gmail.com>
2026-03-24 01:03:58 -0400

    Feat:

---
+++ Api/Controllers/InscriptionsController.cs
@@ -0,0 +1,21 @@
+﻿using Business.DTOs.Requests;
+using Business.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class InscriptionsController(CreateInscription createInscription) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] InscriptionCreateRequest request)
+    {
+        var result = await createInscription.ExecuteAsync(request);
+
+        if (!result.IsSuccess)
+            return BadRequest(result.Errors);
+
+        return Ok(result.Value);
+    }
+}
---
+++ Business/Validators/InscriptionCreateValidator.cs
@@ -0,0 +1,13 @@
+﻿using Business.DTOs.Requests;
+using FluentValidation;
+
+namespace Business.Validators;
+
+public class InscriptionCreateValidator : AbstractValidator<InscriptionCreateRequest>
+{
+    public InscriptionCreateValidator()
+    {
+        RuleFor(x => x.UsuarioId).GreaterThan(0);
+        RuleFor(x => x.CursoId).GreaterThan(0);
+    }
+}
