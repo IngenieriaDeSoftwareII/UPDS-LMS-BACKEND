44bab74536d4422b01c37be67ff328773887cead
rcarlos7 <tj.roberto.olivera.c@upds.net.bo>
2026-03-24 00:54:38 -0400

    Feat: Inscription Repository

---
+++ Data/Repositories/Implementations/InscriptionRepository.cs
@@ -0,0 +1,40 @@
+﻿using Data.Context;
+using Data.Entities;
+using Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories.Implementations
+{
+    public class InscriptionRepository(AppDbContext dbContext) : IInscriptionRepository
+    {
+
+        public async Task<int> CountByCourseAsync(int cursoId)
+        {
+            return await dbContext.Inscriptions
+                .Where(i => i.CursoId == cursoId)
+                .CountAsync();
+        }
+
+        public async Task<Inscription> CreateAsync(Inscription inscripcion)
+        {
+            dbContext.Inscriptions.Add(inscripcion);
+            await dbContext.SaveChangesAsync();
+            return inscripcion;
+        }
+
+        public async Task<Inscription?> GetByUserAndCourseAsync(int usuarioId, int cursoId)
+        {
+            return await dbContext.Inscriptions
+                .Where(i => i.UsuarioId == usuarioId && i.CursoId == cursoId)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Inscription>> GetByUserAsync(int usuarioId)
+        {
+            return await dbContext.Inscriptions
+                .Where(i => i.UsuarioId == usuarioId)
+                .Include(i => i.Curso)
+                .ToListAsync();
+        }
+    }
+}
