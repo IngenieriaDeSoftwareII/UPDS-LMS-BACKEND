0db166459d28cba5e1a789ca965db2abfef0aac4
Cristhian Angelo B. Romero Vega <131317225+RomeroAnge@users.noreply.github.com>
2026-03-28 18:28:25 -0400

    Add CORS support for additional origin

--- Api/Program.cs
+++ Api/Program.cs
@@ -218,7 +218,7 @@ builder.Services.AddAutoMapper(
 
 
 builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
-    policy.WithOrigins("http://localhost:5137")
+    policy.WithOrigins("http://localhost:5173")
            .AllowAnyMethod()
            .AllowAnyHeader()));
 
